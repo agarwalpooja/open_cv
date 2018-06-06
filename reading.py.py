54f701b7a503aebdfc678132fd7c80ef2638d77a
agarwalpooja <poojaag1606@gmail.com>
2018-06-06 12:36:30 +0530

    reading image

---
+++ reading.py.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 28 17:33:01 2018
+
+@author: ASUS
+"""
+import numpy as np
+import cv2 as cv
+img=cv.imread('2.jpeg',0)
+img2=cv.imread('1.jpeg',0)
+print(img)
+print(img2)
