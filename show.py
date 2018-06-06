55ddaa1f8b103625efc582f751619ea6f201bfc2
agarwalpooja <poojaag1606@gmail.com>
2018-06-06 12:32:53 +0530

    showing an image

---
+++ show.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 29 11:03:38 2018
+
+@author: ASUS
+"""
+import numpy as np
+import cv2 as cv
+img=cv.imread('2.jpeg',0)
+cv.imshow('image',img)
+cv.waitKey(0)
+cv.destroyAllWindows()
